f3d6c8481826bae9e69ed1f3775c98f27e7b4ca9
BL9 <35409967+BL9@users.noreply.github.com>
2021-02-11 12:01:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://unruffled-darwin-18378e.netlify.app/'
+        origin: 'https://unruffled-darwin-18378e.netlify.app'
     })
 );
 app.options('*', cors());
